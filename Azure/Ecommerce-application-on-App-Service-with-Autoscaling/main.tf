02b3461f4001388ccd653564952304d2152f5170
Adedamola <adejokuntop@gmail.com>
2024-11-10 22:39:01 -0500

    updating

--- Azure/Ecommerce-application-on-App-Service-with-Autoscaling/main.tf
+++ Azure/Ecommerce-application-on-App-Service-with-Autoscaling/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_service_plan" "webapp" {
 }
 
 resource "azurerm_linux_web_app" "webapp" {
-  name                = "web-app"
+  name                = "web-app-test-molo"
   resource_group_name = azurerm_resource_group.webapp.name
   location            = azurerm_service_plan.webapp.location
   service_plan_id     = azurerm_service_plan.webapp.id
@@ -28,12 +28,16 @@ resource "azurerm_linux_web_app" "webapp" {
     type = "SystemAssigned"
   }
 
-  site_config {}
+  site_config {
+    minimum_tls_version = "1.2"
+    always_on = false
+    ftps_state = "FtpsOnly"
+  }
 }
 
 resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id             = azurerm_linux_web_app.webapp.id
-  repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+  repo_url           = "https://github.com/adejokun/E-commerce-app.git"
   branch             = "master"
   use_manual_integration = true
   use_mercurial      = false
