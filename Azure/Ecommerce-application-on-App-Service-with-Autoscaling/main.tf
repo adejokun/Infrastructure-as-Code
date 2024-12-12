986f13fb72710f996ade1508ba75b8b6cf19e3b7
Adedamola <adejokuntop@gmail.com>
2024-12-12 03:16:59 -0500

    updating

--- Azure/Ecommerce-application-on-App-Service-with-Autoscaling/main.tf
+++ Azure/Ecommerce-application-on-App-Service-with-Autoscaling/main.tf
@@ -13,11 +13,11 @@ resource "azurerm_service_plan" "webapp" {
   resource_group_name = azurerm_resource_group.webapp.name
   location            = azurerm_resource_group.webapp.location
   sku_name            = "B1"
-  os_type             = "Linux"
+  os_type             = "Windows"
 }
 
-resource "azurerm_linux_web_app" "webapp" {
-  name                = "web-app-test-molo-02"
+resource "azurerm_windows_web_app" "webapp" {
+  name                = "web-app-test-molo-03"
   resource_group_name = azurerm_resource_group.webapp.name
   location            = azurerm_service_plan.webapp.location
   service_plan_id     = azurerm_service_plan.webapp.id
@@ -36,8 +36,8 @@ resource "azurerm_linux_web_app" "webapp" {
 }
 
 resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id             = azurerm_linux_web_app.webapp.id
-  repo_url           = "https://github.com/adejokun/E-commerce-app.git"
+  app_id             = azurerm_windows_web_app.webapp.id
+  repo_url           = "https://github.com/SoniaConti/ContosoFinance-Demo-Web.git"
   branch             = "master"
   use_manual_integration = true
   use_mercurial      = false
